455f89a3456f9f694c04520dcea1b4ffae8d3105
sana <sanjana.jagani@gmail.com>
2024-06-18 12:23:12 +0530

    firebase issue on login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,100 +1,342 @@
 import 'package:flutter/material.dart';
-import 'package:mysql1/mysql1.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:realme/home.dart';
 
-class LoginPage extends StatefulWidget {
-  LoginPage({Key? key}) : super(key: key);
-
+class Login extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _LoginState createState() => _LoginState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  TextEditingController _usernameController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-  late String _errorMessage;
+class _LoginState extends State<Login> {
+  bool _obscureText = true;
+  FocusNode _emailFocusNode = FocusNode();
+  FocusNode _passwordFocusNode = FocusNode();
+  bool _isGoogleSigningIn = false;
 
-  @override
-  void initState() {
-    super.initState();
-    _errorMessage = '';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<void> _signInWithGoogle() async {
+    setState(() {
+      _isGoogleSigningIn = true;
+    });
+
+    try {
+      final GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
+      if (googleSignInAccount != null) {
+        final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken,
+        );
+
+        final UserCredential authResult = await _auth.signInWithCredential(credential);
+        final User? user = authResult.user;
+
+        if (user != null && user.providerData.any((info) => info.providerId == 'google.com')) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => Home()),
+          );
+        }
+      }
+    } catch (error) {
+      print('Error signing in with Google: $error');
+      // Handle sign-in errors
+    } finally {
+      setState(() {
+        _isGoogleSigningIn = false;
+      });
+    }
   }
 
-  Future<void> _login() async {
-    final String username = _usernameController.text.trim();
-    final String password = _passwordController.text.trim();
-
-    final settings = ConnectionSettings(
-        host: '10.0.2.2',
-        port: 3306,
-        user: 'user',
-        password: '1',
-        db: 'if0_36371201_test',
-        timeout: Duration(hours: 1)
-    );
-
-    final MySqlConnection conn = await MySqlConnection.connect(settings).timeout(Duration(seconds: 10));
-    try {
-      final results = await conn.query(
-          'SELECT * FROM users WHERE email = ? AND password = ?',
-          [username, password]);
-      print(results);
-      if (results.isNotEmpty) {
-        // Navigate to home screen or do something on successful login
-        print('Login successful');
-        Navigator.push(context, MaterialPageRoute(builder: (context)=>Home()));
-      } else {
-        setState(() {
-          _errorMessage = 'Invalid username or password';
-        });
-      }
-    } catch (e) {
-      print('Error during login: $e');
-      setState(() {
-        _errorMessage = 'An error occurred, please try again later';
-      });
-    } finally {
-      await conn.close();
-    }
+  @override
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 60,
+            left: 36,
+            child: Container(
+              width: 332,
+              height: 175,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromRGBO(245, 251, 251, 1),
+              ),
+              child: Center(
+                child: Image.asset('assets/images/loginmed.png'),
               ),
             ),
-            SizedBox(height: 14),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: _login,
-              child: Text('Login'),
-            ),
-            if (_errorMessage.isNotEmpty)
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                child: Text(
-                  _errorMessage,
-                  style: TextStyle(color: Colors.red),
+          ),
+          // Login Container
+          Positioned.fill(
+            top: 231,
+            child: SingleChildScrollView(
+              child: Center(
+                child: Container(
+                  width: 369,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromRGBO(0, 0, 0, 0.25),
+                        offset: Offset(1, 1),
+                        blurRadius: 4,
+                      ),
+                    ],
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: Center(
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Text(
+                                'Login',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Color.fromRGBO(0, 0, 0, 1),
+                                  fontFamily: 'Poppins',
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                              Positioned(
+                                bottom: -8,
+                                left: 0,
+                                right: 0,
+                                child: Container(
+                                  height: 11,
+                                  width: 200,
+                                  color: Color(0xFF05D5CD),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 18),
+
+                      // Aligned Text Section
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0), // Adjust left padding here
+                              child: Text(
+                                'Welcome Back',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Color.fromRGBO(0, 0, 0, 1),
+                                  fontFamily: 'Poppins',
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0), // Adjust left padding here
+                              child: Text(
+                                'Fill out the information below in order to \n access your account.',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Color.fromRGBO(87, 99, 108, 1),
+                                  fontFamily: 'Poppins',
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+
+                            // Email and Password Fields
+                            Column(
+                              children: [
+                                TextField(
+                                  focusNode: _emailFocusNode,
+                                  decoration: InputDecoration(
+                                    labelText: 'Email',
+                                    border: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Colors.grey[400]!,
+                                        width: 2,
+                                      ),
+                                      borderRadius: BorderRadius.circular(40),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: _emailFocusNode.hasFocus ? Color(0xFF05D5CD) : Colors.grey[400]!,
+                                        width: 2,
+                                      ),
+                                      borderRadius: BorderRadius.circular(40),
+                                    ),
+                                    contentPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+                                  ),
+                                ),
+                                SizedBox(height: 16),
+                                Stack(
+                                  alignment: Alignment.centerRight,
+                                  children: [
+                                    TextField(
+                                      focusNode: _passwordFocusNode,
+                                      obscureText: _obscureText,
+                                      decoration: InputDecoration(
+                                        labelText: 'Password',
+                                        border: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                            color: Colors.grey[400]!,
+                                            width: 2,
+                                          ),
+                                          borderRadius: BorderRadius.circular(40),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                            color: _passwordFocusNode.hasFocus ? Color(0xFF05D5CD) : Colors.grey[400]!,
+                                            width: 2,
+                                          ),
+                                          borderRadius: BorderRadius.circular(40),
+                                        ),
+                                        contentPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+                                      ),
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _obscureText = !_obscureText;
+                                        });
+                                      },
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(right: 12.0),
+                                        child: Icon(
+                                          _obscureText ? Icons.visibility_off : Icons.visibility,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+
+                            SizedBox(height: 20),
+
+                            // Centered Login Button
+                            Center(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => Home()),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  padding: EdgeInsets.symmetric(horizontal: 64, vertical: 16),
+                                ),
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Poppins',
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Column(
+                              children: [
+                                Text(
+                                  'Or sign in with',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(88, 100, 109, 1),
+                                    fontFamily: 'Poppins',
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                Center( // Center the buttons and text
+                                  child: Column(
+                                    children: [
+                                      OutlinedButton(
+                                        onPressed: _signInWithGoogle,
+                                        style: OutlinedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(30),
+                                          ),
+                                          side: BorderSide(
+                                            color: Color.fromRGBO(224, 236, 236, 1),
+                                            width: 2,
+                                          ),
+                                          padding: EdgeInsets.symmetric(horizontal: 43, vertical: 16),
+                                        ),
+                                        child: Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Image.asset('assets/images/Google - Original.png', width: 26, height: 27),
+                                            SizedBox(width: 10),
+                                            Text(
+                                              'Continue With Google',
+                                              style: TextStyle(
+                                                color: Colors.black,
+                                                fontFamily: 'Poppins',
+                                                fontSize: 14,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(height: 12),
+                                      TextButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          'Forgot Password?',
+                                          style: TextStyle(
+                                            color: Color.fromRGBO(16, 21, 24, 1),
+                                            fontFamily: 'Poppins',
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(height: 15),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
